4cd5975d2ce235a7e2c49ed610924d5dad1d3f9c
extremeheat <extreme@protonmail.ch>
2022-01-07 15:53:57 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,8 @@ declare module "minecraft-bedrock-server" {
   // Starts the server
   // Options is an array of server.properties options.
   // `options.path` is the path to where the server will be started.
-  function startServer(version, onStart, options: { path: string }): Promise<ChildProcess>
+  function startServer(version, onStart, options): Promise<ChildProcess>
 
   // Starts the server and waits
-  function startServerAndWait(version, withTimeout, options: { path: string }): Promise<ChildProcess>
+  function startServerAndWait(version, withTimeout, options): Promise<ChildProcess>
 }
