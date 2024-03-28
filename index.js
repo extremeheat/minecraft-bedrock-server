003c874adaf2c8571e424156e813f441c44ff725
extremeheat <extreme@protonmail.ch>
2024-03-28 21:28:04 +0000

    Start work on refactor

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 const http = require('https')
 const fs = require('fs')
 const cp = require('child_process')
-const { resolve } = require('path')
-const debug = require('debug')('minecraft-protocol')
-const { getFiles, waitFor } = require('./util')
+const debug = process.env.CI ? console.debug : require('debug')('minecraft-bedrock-server')
+const https = require('https')
 
-const head = (url) => new Promise((resolve, reject) => http.request(url, { method: 'HEAD' }, resolve).on('error', reject).end())
-const get = (url, out) => cp.execSync(`curl -o ${out} ${url}`)
-
-// Get the latest versions
-// TODO: once we support multi-versions
-function fetchLatestStable () {
-  get('https://raw.githubusercontent.com/minecraft-linux/mcpelauncher-versiondb/master/versions.json', 'versions.json')
-  const versions = JSON.parse(fs.readFileSync('./versions.json'))
-  const latest = versions[versions.length - 1]
-  return latest.version_name
+function head (url) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(url, { method: 'HEAD', timeout: 1000 }, resolve)
+    req.on('error', reject)
+    req.on('timeout', () => { req.destroy(); debug('HEAD request timeout'); reject(new Error('timeout')) })
+    req.end()
+  })
 }
 
-// Download + extract vanilla server and enter the directory
-async function download (os, version, dir) {
-  dir = dir || ('./bds-' + version)
-  const verStr = version.split('.').slice(0, 3).join('.')
+function get (url, outPath) {
+  const file = fs.createWriteStream(outPath)
+  return new Promise((resolve, reject) => {
+    https.get(url, { timeout: 1000 * 20 }, response => {
+      if (response.statusCode !== 200) return reject(new Error('Server returned code ' + response.statusCode))
+      response.pipe(file)
+      file.on('finish', () => {
+        file.close()
+        resolve()
+      })
+    })
+  })
+}
 
-  dir = resolve(dir)
-  if (fs.existsSync(dir) && getFiles(dir).length) {
+let lock = false
+
+// Download + extract vanilla server and enter the directory
+async function download (os, version, root, path) {
+  if (lock) {
+    throw Error('Already downloading server')
+  }
+  lock = true
+  process.chdir(root)
+  const verStr = version.split('.').slice(0, 3).join('.')
+  const dir = path || 'bds-' + version
+
+  if (fs.existsSync(dir) && fs.readdirSync(dir).length) {
     process.chdir(dir) // Enter server folder
+    debug('Already downloaded', version)
+    lock = false
     return verStr
   }
-
   try { fs.mkdirSync(dir) } catch { }
 
   process.chdir(dir) // Enter server folder
@@ -37,8 +54,9 @@ async function download (os, version, dir) {
 
   for (let i = 0; i < 8; i++) { // Check for the latest server build for version (major.minor.patch.BUILD)
     const u = url(os, `${verStr}.${String(i).padStart(2, '0')}`)
-    debug('Opening', u)
-    const ret = await head(u)
+    debug('Opening', u, Date.now())
+    let ret
+    try { ret = await head(u) } catch (e) { continue }
     if (ret.statusCode === 200) {
       found = u
       debug('Found server', ret.statusCode)
@@ -47,13 +65,21 @@ async function download (os, version, dir) {
   }
   if (!found) throw Error('did not find server bin for ' + os + ' ' + version)
   console.info('🔻 Downloading', found)
-  get(found, 'bds.zip')
+  await get(found, 'bds.zip')
   console.info('⚡ Unzipping')
   // Unzip server
-  if (process.platform === 'linux') cp.execSync('unzip bds.zip && chmod +777 ./bedrock_server')
+  if (process.platform === 'linux') cp.execSync('unzip -u bds.zip && chmod +777 ./bedrock_server')
   else cp.execSync('tar -xf bds.zip')
+  lock = false
   return verStr
 }
+
+// function erase (version, root, path) {
+//   process.chdir(root)
+//   const dir = path || 'bds-' + version
+//   debug('Removing server', root + path)
+//   fs.rmSync(dir, { recursive: true, force: true })
+// }
 
 const defaultOptions = {
   'level-generator': '2',
@@ -75,20 +101,25 @@ function run (inheritStdout = true) {
   return cp.spawn(exe, inheritStdout ? { stdio: 'inherit' } : {})
 }
 
+let lastHandle
+
 // Run the server
 async function startServer (version, onStart, options = {}) {
   const os = process.platform === 'win32' ? 'win' : process.platform
   if (os !== 'win' && os !== 'linux') {
     throw Error('unsupported os ' + os)
   }
+
   const currentDir = process.cwd()
+  // Take the options.path and determine if it's an absolute path or not
+  const path = options.path
+  const pathRoot = options.root || __dirname
 
-  await download(os, version, options.path)
+  await download(os, version, pathRoot, path) // and enter the directory
+  debug('Configuring server', version)
   configure(options)
-  const handle = run(!onStart)
-
-  process.chdir(currentDir)
-
+  debug('Starting server', version)
+  const handle = lastHandle = run(!onStart)
   handle.on('error', (...a) => {
     console.warn('*** THE MINECRAFT PROCESS CRASHED ***', a)
     handle.kill('SIGKILL')
@@ -102,20 +133,46 @@ async function startServer (version, onStart, options = {}) {
     handle.stdout.pipe(process.stdout)
     handle.stderr.pipe(process.stdout)
   }
+  process.chdir(currentDir)
   return handle
 }
 
 // Start the server and wait for it to be ready, with a timeout
-async function startServerAndWait (version, withTimeout, options) {
-  if (isNaN(withTimeout)) throw Error('timeout must be a number')
+function startServerAndWait (version, withTimeout, options) {
   let handle
-  await waitFor(async res => {
-    handle = await startServer(version, res, options)
-  }, withTimeout, () => {
-    handle?.kill()
-    throw new Error('Server did not start on time ' + withTimeout) // Make sure ports are not blocked.
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      handle?.kill()
+      reject(new Error(`Server did not start on time (${withTimeout}ms, now ${Date.now()})`))
+    }, withTimeout)
+
+    startServer(version, function onReady () {
+      clearTimeout(timeout)
+      resolve(handle)
+    }, options).then((h) => {
+      handle = h
+    }).catch(reject)
   })
-  return handle
 }
 
-module.exports = { fetchLatestStable, startServer, startServerAndWait }
+// Start the server and wait for it to be ready, with a timeout, and retry once
+async function startServerAndWait2 (version, withTimeout, options) {
+  try {
+    return await startServerAndWait(version, withTimeout, options)
+  } catch (e) {
+    console.log(e)
+    console.log('^ Trying once more to start server in 10 seconds...')
+    lastHandle?.kill()
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    // Remove the server and try again
+    const currentDir = process.cwd()
+    process.chdir(options.root || __dirname)
+    const path = options.path ? options.path : 'bds-' + version
+    debug('Removing server', path)
+    fs.rmSync(path, { recursive: true, force: true })
+    process.chdir(currentDir)
+    return await startServerAndWait(version, withTimeout, options)
+  }
+}
+
+module.exports = { startServer, startServerAndWait, startServerAndWait2 }
